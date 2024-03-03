d9f12d4fb10b38c2a9e3c449a4afb4b8cede1fe5
rayan2228 <rayanh.dev@gmail.com>
2024-03-04 00:28:06 +0600

    register done

--- blogify/src/hooks/useAuth.js
+++ blogify/src/hooks/useAuth.js
@@ -1,7 +1,9 @@
-import { useContext } from "react";
+import { useContext, useDebugValue } from "react";
 import { AuthContext } from "../context";
 
 const useAuth = () => {
+    const { auth } = useContext(AuthContext)
+    useDebugValue(auth, auth => auth?.user ? "user logged in" : "user logged out")
     return useContext(AuthContext)
 }
 export default useAuth
--- blogify/src/pages/Register.jsx
+++ blogify/src/pages/Register.jsx
@@ -2,22 +2,66 @@ import Container from "../components/layouts/Container";
 import { Link, useNavigate } from "react-router-dom";
 import InputField from "../components/layouts/InputField";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 const Register = () => {
   const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm();
-  const formSubmit = (formData) => {
-    console.log(formData);
-    navigate("/login");
+  const formSubmit = async (formData) => {
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_BASEURL}auth/register`,
+        formData
+      );
+      if (res.status === 201) {
+        navigate("/login");
+      }
+    } catch (error) {
+      setError("root.random", {
+        type: "random",
+        message: error.response?.data?.error,
+      });
+    }
   };
   return (
     <main>
       <Container>
         <div className="w-full md:w-1/2 mx-auto bg-[#030317] p-8 rounded-md mt-12">
           <h2 className="mb-6 text-2xl font-bold">Register</h2>
+          {errors.root?.random?.message && (
+            <div
+              className="relative px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded"
+              role="alert"
+            >
+              <span className="block sm:inline">
+                {errors.root?.random?.message}
+              </span>
+              <span
+                className="absolute top-0 bottom-0 right-0 px-4 py-3"
+                onClick={() =>
+                  setError("root.random", {
+                    type: "random",
+                    message: "",
+                  })
+                }
+              >
+                <svg
+                  className="w-6 h-6 text-red-500 fill-current"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+              </span>
+            </div>
+          )}
+
           <form onSubmit={handleSubmit(formSubmit)} autoComplete="off">
             <InputField
               className={"mb-6"}
