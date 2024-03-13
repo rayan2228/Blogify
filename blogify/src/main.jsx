7a1eccec4503984f330c28beac1493b62ea370ca
rayan2228 <rhrayan2228@gmail.com>
2024-03-13 08:55:21 +0600

    blogs provider

--- blogify/src/main.jsx
+++ blogify/src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import BlogsProvider from "./providers/BlogsProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Router>
-        <App />
-      </Router>
-    </AuthProvider>
+    <BlogsProvider>
+      <AuthProvider>
+        <Router>
+          <App />
+        </Router>
+      </AuthProvider>
+    </BlogsProvider>
   </React.StrictMode>
 );
