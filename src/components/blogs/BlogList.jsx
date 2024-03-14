96ebfd8e896dc966c11b02e70ca0928ce391a404
rayan2228 <rhrayan2228@gmail.com>
2024-03-14 14:32:50 +0600

    link issue

--- src/components/blogs/BlogList.jsx
+++ src/components/blogs/BlogList.jsx
@@ -8,66 +8,66 @@ const BlogList = ({ blog }) => {
   const { state } = useProfile();
   const { auth } = useAuth();
   return (
-    <Link className="blog-card" to={`/single-blog/${blog?.id}`}>
+    <div className="blog-card">
       {blog?.thumbnail && (
-        // <Link to={`/single-blog/${blog?.id}`}>
-        <Img
-          className="blog-thumb"
-          src={`${import.meta.env.VITE_IMAGE_BASEURL}blog/${blog.thumbnail}`}
-          alt={blog.thumbnail}
-        />
-        // </Link>
+        <Link to={`/single-blog/${blog?.id}`}>
+          <Img
+            className="blog-thumb"
+            src={`${import.meta.env.VITE_IMAGE_BASEURL}blog/${blog.thumbnail}`}
+            alt={blog.thumbnail}
+          />
+        </Link>
       )}
       <div className="relative mt-2">
-        {/* <Link to={`/single-blog/${blog?.id}`}> */}
-        <h3 className="text-xl text-slate-300 lg:text-2xl">{blog.title}</h3>
-        {/* </Link> */}
-        {/* <Link to={`/single-blog/${blog?.id}`}> */}
-        <p className="mt-1 mb-6 text-base text-slate-500">
-          {`${blog.content.slice(0, 180)} ...`}
-        </p>
-        {/* </Link> */}
-        {/* Meta Information */}
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-2 capitalize">
-            <Link to={`/profile/${blog?.author?.id}`}>
-              {blog.author?.avatar ? (
-                <Img
-                  src={`${import.meta.env.VITE_IMAGE_BASEURL}/avatar/${
-                    state?.user?.id === blog.author?.id
-                      ? state?.user?.avatar
-                      : blog.author?.avatar
-                  }`}
-                  className={"rounded-full w-8 h-8 object-cover"}
-                />
-              ) : (
-                <div className="text-white bg-indigo-600 avater-img">
-                  <span className="">{blog?.author?.firstName.charAt(0)}</span>
+        <Link to={`/single-blog/${blog?.id}`}>
+          <h3 className="text-xl text-slate-300 lg:text-2xl">{blog.title}</h3>
+          <p className="mt-1 mb-6 text-base text-slate-500">
+            {`${blog.content.slice(0, 180)} ...`}
+          </p>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-2 capitalize">
+              <Link to={`/profile/${blog?.author?.id}`}>
+                {blog.author?.avatar ? (
+                  <Img
+                    src={`${import.meta.env.VITE_IMAGE_BASEURL}/avatar/${
+                      state?.user?.id === blog.author?.id
+                        ? state?.user?.avatar
+                        : blog.author?.avatar
+                    }`}
+                    className={"rounded-full w-8 h-8 object-cover"}
+                  />
+                ) : (
+                  <div className="text-white bg-indigo-600 avater-img">
+                    <span className="">
+                      {blog?.author?.firstName.charAt(0)}
+                    </span>
+                  </div>
+                )}
+              </Link>
+              <div>
+                <h5 className="text-sm text-slate-500">
+                  <Link to={`/profile/${blog?.author?.id}`}>
+                    {blog.author?.firstName} {blog.author?.lastName}
+                  </Link>
+                </h5>
+                <div className="flex items-center text-xs text-slate-700">
+                  <span>{getDateFormat(blog?.createdAt)}</span>
                 </div>
-              )}
-            </Link>
-            <div>
-              <h5 className="text-sm text-slate-500">
-                <Link to={`/profile/${blog?.author?.id}`}>
-                  {blog.author?.firstName} {blog.author?.lastName}
-                </Link>
-              </h5>
-              <div className="flex items-center text-xs text-slate-700">
-                <span>{getDateFormat(blog?.createdAt)}</span>
               </div>
             </div>
+            <div className="px-2 py-1 text-sm text-slate-700">
+              <span>{blog.likes.length} Likes</span>
+            </div>
           </div>
-          <div className="px-2 py-1 text-sm text-slate-700">
-            <span>{blog.likes.length} Likes</span>
-          </div>
-        </div>
+        </Link>
+        {/* Meta Information */}
         {/* action dot */}
         {auth?.user?.id === blog.author?.id && (
           <BlogActions blogDetails={blog} />
         )}
         {/* action dot ends */}
       </div>
-    </Link>
+    </div>
   );
 };
 
